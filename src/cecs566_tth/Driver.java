9b1ec605a690cc083ce43a21eb8e9210f890d882
Sam Dicken <sjdick04@louisville.edu>
2014-06-10 15:15:44 -0400

    Remove comments and fix function parameter for generating hash.

--- src/cecs566_tth/Driver.java
+++ src/cecs566_tth/Driver.java
@@ -5,10 +5,9 @@ public class Driver
 {
 	public static void main(String [] args)
 	{
-//		System.out.println(Utils.translateTotalToLetters(Utils.compressionFunction(Utils.convertMessageTo4x4Matrices("I leave twenty million dollars to my friendly cousin Bill."))));
-//		Integer [] targetNumbers = { 6,25,17,22 };
 		String matchingHash = Utils.findMatchingHash("BFQG");
 		System.out.println(matchingHash);
 		System.out.println(Utils.convertMessageTo4x4Matrices(matchingHash));
+		System.out.println(Utils.translateHashValueToLetters(Utils.compressionFunction(Utils.convertMessageTo4x4Matrices(matchingHash))));
 	}
 }
